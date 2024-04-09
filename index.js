af85f36ad9f319a7c6b8ccf8fea54c8151c90540
Yury Butskevich <124175026+YourunB@users.noreply.github.com>
2024-04-09 18:52:39 +0300

    refactor: add class to link

--- index.js
+++ index.js
@@ -186,7 +186,7 @@ const links = {
     </div>
   </div>
   
-  <a href="https://codepen.io/BxYura/pens/public?cursor=ZD0wJm89MSZwPTEmdj02OTkwMDQ0MQ==">...more projects</a>
+  <a class="more-project" href="https://codepen.io/BxYura/pens/public?cursor=ZD0wJm89MSZwPTEmdj02OTkwMDQ0MQ==">...more projects</a>
   `,
   contacts: `
     <h2 class="show">Ready to do something unusual and interesting!</h2>
